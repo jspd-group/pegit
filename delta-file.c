af741acef68e5d6847154a2e4b2c930361f05592
Prince Dhaliwal <dhaliwalprince@hotmail.com>
2016-02-16 00:24:35 +0530

    Migrated deltafile to delta-file.h

---
+++ delta-file.c
@@ -0,0 +1,51 @@
+#include "delta-file.h"
+#include "strbuf.h"
+
+size_t count_lines(struct strbuf *buf)
+{
+	char *str = buf->buf;
+	size_t count;
+
+	while (*str)
+		if (*str++ == '\n')
+			count++;
+
+	if (*(--str) != '\n')
+		count++;
+
+	return count;
+}
+
+int deltafile_init(struct deltafile *df, size_t size, char delim)
+{
+	df->arr = (struct strbuf*)malloc(sizeof(struct deltafile) * size);
+	if (!df->arr)
+		die("Out of memory.\n");
+
+	df->size = size;
+	df->delim = delim;
+}
+
+int deltafile_init_filespec(struct deltafile *df, struct filespec *fs, char d)
+{
+	struct strbuf buf;
+	strbuf_init(&buf, fs->length);
+	filespec_read_safe(fs, &buf);
+	return deltafile_init_strbuf(df, &buf, d);
+}
+
+int deltafile_init_strbuf(struct deltafile *df, struct strbuf *buf, char delim)
+{
+	size_t lines = count_lines(buf);
+	deltafile_init(df, lines, delim);
+	df->size = strbuf_split(buf, df->arr, delim) + 1;
+	return 0;
+}
+
+void deltafile_free(struct deltafile *df)
+{
+	for (int i = 0; i < df->size; i++) 
+		strbuf_release(df->arr + i);
+	free(df->arr);
+	df->size = 0;
+}
---
+++ delta-file.h
@@ -0,0 +1,46 @@
+#ifndef DELTA_FILE_H_
+#define DELTA_FILE_H_
+
+#include "file.h"
+#include "util.h"
+#include "strbuf.h"
+
+/**
+ * store the whole file in the strbuf array. Each element in the array
+ * corresponds to a particular line in the file. Using such format is
+ * useful for making the delta faster (a little). It may take more memory.
+ */
+struct deltafile {
+    struct strbuf *arr;    /* main array */
+    size_t size;        /* size of the array */
+    char delim;        /* delimiter on which we are dividing the lines */
+};
+
+/**
+ * deltafile routines
+ * -------------------------------
+ */
+
+/**
+ * Initialise a deltafile by specifying its size
+ */
+extern int deltafile_init(struct deltafile *df, size_t size, char delim);
+
+/**
+ * Initialise  deltafile from a given filespec
+ */
+extern int deltafile_init_filespec(struct deltafile *df,
+								   struct filespec *fs, char delim);
+
+/**
+ * Initialise the deltafile from a strbuf
+ */
+extern int deltafile_init_strbuf(struct deltafile *df,
+								 struct strbuf *buf, char delim);
+
+/**
+ * free the memory
+ */
+extern void deltafile_free(struct deltafile *df);
+
+#endif // DELTA_FILE_H_
