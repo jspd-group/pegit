cf9f3daecbf7c83341f44dcd74272181ce6c3f52
Prince Dhaliwal <dhaliwalprince@hotmail.com>
2016-02-15 21:03:15 +0530

    Added hash utilities to the repo:

---
+++ sha1-inl.h
@@ -0,0 +1,19 @@
+#ifndef SHA1_INL_H_
+#define SHA1_INL_H_
+
+#include "util.h"
+#include "sha1.h"
+#include "strbuf.h"
+
+#include <string.h>
+
+void sha1(struct strbuf in, char out[20]) {
+    sha1nfo ctx;
+    sha1_init(&ctx);
+    sha1_write(&ctx, in.buf, in.len);
+
+    strncpy(sha1_result(&ctx), out, 20);
+}
+
+
+#endif /* SHA1_INL_H_ */
