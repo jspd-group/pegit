5d8033a9206decb3484cc324364e067094b561c3
Prince Dhaliwal <dhaliwalprince@hotmail.com>
2016-03-05 14:25:43 +0530

    Added support for zlib replacing miniz

--- include/mz.h
+++ include/mz.h
@@ -1,7 +1,7 @@
 #ifndef MZ_WRAPPER_H_
 #define MZ_WRAPPER_H_
 
-#include "../miniz/miniz.h"
+#include <zlib.h>
 #include "strbuf.h"
 
 #ifdef compress
@@ -13,11 +13,11 @@
 #endif
 
 /*
- * this function compresses the src to dest to a level
+ * this function compresses the src to dest to a given level
  * and returns 0 if successfully compressed otherwise returns -1
  * use level 0 for no compression, 9 for best compression
  */
-extern int compress(struct strbuf *src, struct strbuf *dest, int level);
+extern int __compress__(struct strbuf *src, struct strbuf *dest, int level);
 
 /*
  * compress the src to dest with Z_DEFAULT_COMPRESSION level
@@ -25,10 +25,10 @@ extern int compress(struct strbuf *src, struct strbuf *dest, int level);
 extern int compress_default(struct strbuf *src, struct strbuf *dest);
 
 /*
- * uncompresses a string buffer from the src and put the result
+ * decompresses a string buffer from the src and put the result
  * in dest buffer. Returns 0 if successful otherwise -1
  */
-extern int uncompress(struct strbuf *src, struct strbuf *dest);
+extern int decompress(struct strbuf *src, struct strbuf *dest);
 
 
 #endif /* MZ_WRAPPER_H_ */
