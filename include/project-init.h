fc8489a55b762adf4cb94656409bf9be02fd557f
Prince Dhaliwal <dhaliwalprince@hotmail.com>
2016-03-10 22:20:49 +0530

    Added project-init files

---
+++ include/project-init.h
@@ -0,0 +1,10 @@
+#ifndef PROJECT_INIT_H_
+#define PROJECT_INIT_H_
+
+/**
+ * initialise the project whose description in the provided in the arguments
+ * arguments include project name essentially.
+ */
+extern int initialize_empty_project(int argc, char *argv[]);
+
+#endif
