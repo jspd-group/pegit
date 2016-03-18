e7ea573b31990aa5b0f150465f3cec61e958ab93
Prince Dhaliwal <dhaliwalprince@hotmail.com>
2016-03-18 22:50:55 +0530

    Fixed unexpected crashing of delta

--- src/delta-file.c
+++ src/delta-file.c
@@ -58,6 +58,8 @@ int deltafile_init_filespec(struct deltafile *df, struct filespec *fs, char d)
 
 int deltafile_init_strbuf(struct deltafile *df, struct strbuf *buf, char delim)
 {
+    strbuf_init(&df->file, buf->len);
+    strbuf_addbuf(&df->file, buf);
     size_t lines = count_lines(buf);
     deltafile_init(df, lines + 1, delim);
     df->size = split_in_place(buf, df->arr, delim);
