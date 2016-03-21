d543a86dc1808370f3034a6f64b9712237428445
Prince Dhaliwal <dhaliwalprince@hotmail.com>
2016-03-21 23:39:03 +0530

    Updated definition of die Macro

--- include/util.h
+++ include/util.h
@@ -28,12 +28,6 @@
  */
 #define HASH_SIZE 20
 
-#define die(...)                                                               \
-    do {                                                                       \
-        printf(__VA_ARGS__);                                                   \
-        exit(1);                                                               \
-    } while(0);
-
 #define DEBUG 0
 
 #define comment_line_char '#'
@@ -91,15 +85,15 @@ extern int __compress__(struct strbuf *src, struct strbuf *dest, int level);
 extern int compress_default(struct strbuf *src, struct strbuf *dest);
 extern int decompress(struct strbuf *src, struct strbuf *dest);
 
-#define PROGRAM             program
-#define PEG_DIR             ".peg"
-#define PEG_NAME            "peg"
-#define CACHE_DIR           "cache"
-#define DB_DIR              "db"
-#define COMMIT_DIR          "commit"
-#define DESCRIPTION_FILE    ".peg/desc"
-#define CACHE_INDEX_FILE    ".peg/cache/cache.idx"
-#define CACHE_PACK_FILE     ".peg/cache/cache.pack"
+#define PROGRAM program
+#define PEG_DIR ".peg"
+#define PEG_NAME "peg"
+#define CACHE_DIR "cache"
+#define DB_DIR "db"
+#define COMMIT_DIR "commit"
+#define DESCRIPTION_FILE ".peg/desc"
+#define CACHE_INDEX_FILE ".peg/cache/cache.idx"
+#define CACHE_PACK_FILE ".peg/cache/cache.pack"
 #define FILE_INDEX_FILE ".peg/db/db.idx"
 #define COMMIT_INDEX_FILE ".peg/commit/commit.idx"
 #define PACK_FILE ".peg/db/db.pack"
@@ -123,5 +117,28 @@ extern int decompress(struct strbuf *src, struct strbuf *dest);
  * text attributes
  */
 #define BOLD "\x1b[1m"
+
+#ifndef fatal
+#define fatal(...)                                                             \
+    do {                                                                       \
+        fprintf(stderr, RED);                                                  \
+        fprintf(stderr, "fatal");                                              \
+        fprintf(stderr, RESET);                                                \
+        fprintf(stderr, ": ");                                                 \
+        fprintf(stderr, __VA_ARGS__);                                          \
+    } while (0)
+#endif
+
+#ifndef die
+#define die(...)                                                          \
+    do {                                                                       \
+        fprintf(stderr, RED);                                                  \
+        fprintf(stderr, "fatal");                                              \
+        fprintf(stderr, RESET);                                                \
+        fprintf(stderr, ": ");                                                 \
+        fprintf(stderr, __VA_ARGS__);                                     \
+        exit(1);                                                               \
+    } while (0)
+#endif
 
 #endif
