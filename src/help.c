cb67a181518b10700c6ad669b6d7fc5e0abdd459
Prince Dhaliwal <dhaliwalprince@hotmail.com>
2016-04-16 09:02:00 +0530

    Nicely formatted the code

--- src/help.c
+++ src/help.c
@@ -1,38 +1,45 @@
-#include"help.h"
-#include<string.h>
-#include<stdio.h>
+#include "help.h"
 
-#include<windows.h>
+#include "util.h"
+#include <string.h>
+#include <stdio.h>
+
+#include <windows.h>
 
 void display_help(char *str)
 {
-	if(strcmp(str,"insert")==0)
+	if(strcmp(str, "insert")==0)
 	{
-		printf("Opening the insert help page ....\n");
-		ShellExecute(NULL,"open",".\\help-pages\\peg_insert.html",NULL,NULL,SW_SHOWNORMAL);
-		
+		printf("Opening the peg-insert help page ....\n");
+		ShellExecute(NULL, "open", ".\\help-pages\\peg_insert.html", NULL, NULL,
+            SW_SHOWNORMAL);
+
 	}
 	else if(strcmp(str,"create")==0)
 	{
 		printf("Opening the peg-create help page ....\n");
-		ShellExecute(NULL,"open",".\\help-pages\\create_project.html",NULL,NULL,SW_SHOWNORMAL);
+		ShellExecute(NULL, "open", ".\\help-pages\\create_project.html", NULL, NULL,
+            SW_SHOWNORMAL);
 
 	}
 	else if(strcmp(str,"commit")==0)
 	{
 		printf("Opening the peg-commit help page ....\n");
-		ShellExecute(NULL,"open",".\\help-pages\\peg_commit.html",NULL,NULL,SW_SHOWNORMAL);
+		ShellExecute(NULL, "open", ".\\help-pages\\peg_commit.html", NULL, NULL,
+            SW_SHOWNORMAL);
 
 	}
 	else if(strcmp(str,"about")==0)
 	{
 		printf("Opening the peg-about help page ....\n");
-		ShellExecute(NULL,"open",".\\help-pages\\about.html",NULL,NULL,SW_SHOWNORMAL);
+		ShellExecute(NULL, "open", ".\\help-pages\\about.html", NULL, NULL,
+            SW_SHOWNORMAL);
 
 	}
-	else 
+	else
 	{
-		printf("Entered string is %s [404 : No such keyword] \nEnter peg help all to list all commands",str);
-		//ShellExecute(NULL,"open",".\\help-pages\\about.html",NULL,NULL,SW_SHOWNORMAL);	
+		die("Entered string is %s [404: No such keyword]\n"
+            "Enter 'peg --help' to list all commands\n", str);
+		//ShellExecute(NULL,"open",".\\help-pages\\about.html",NULL,NULL,SW_SHOWNORMAL);
 	}
 }
